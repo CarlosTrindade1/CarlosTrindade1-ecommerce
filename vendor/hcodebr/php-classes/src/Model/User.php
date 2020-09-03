2b5db9f303bc5678e7df9f28421e643420ee0485
Carlos Trindade <carlos.trindade@estudante.ifms.edu.br>
2020-09-03 10:32:34 -0400

    Site - minha conta

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -10,6 +10,7 @@
         const SECRET_IV = "HcodePhp7_Secret";
         const ERROR = "UserError";
         const ERROR_REGISTER = "UserErrorRegister";
+        const SUCCESS = "UserSuccess";
 
 
         public static function getFromSession(){
@@ -298,6 +299,22 @@
             ]);
 
             return (count($results) > 0);
+        }
+
+        public static function setSuccess($msg){
+            $_SESSION[User::SUCCESS] = $msg;
+        }
+
+        public static function getSuccess(){
+            $msg = (isset($_SESSION[User::SUCCESS])) && $_SESSION[User::SUCCESS] ? $_SESSION[User::SUCCESS] : '';
+
+            User::clearSuccess();
+
+            return $msg;
+        }
+
+        public static function clearSuccess(){
+            $_SESSION[User::SUCCESS] = NULL;
         }
     }
 ?>
